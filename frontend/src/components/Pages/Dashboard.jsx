d62d66d701d8d720c8eacbfb0b66bd743156c4b4
andres9722 <andres_9722@outlook.com>
2018-07-01 15:00:22 -0500

    including firebase for authentication

--- frontend/src/components/Pages/Dashboard.jsx
+++ frontend/src/components/Pages/Dashboard.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import Menu from '../Menu/Menu.jsx'
-import { AuthConsumer } from '../../providers/AuthProvider.jsx'
 
 const Dashboard = () => (
   <Menu />
